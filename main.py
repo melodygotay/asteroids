33a88bd181fda8f8b0ff8b9791d294dde2a7088e
melodygotay <melody.gotay@upr.edu>
2024-08-21 22:13:15 +0200

    Ch2 Pt8

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ import pygame
 from constants import *
 from player import Player
 
-x = SCREEN_WIDTH / 2
-y = SCREEN_HEIGHT / 2
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -4,6 +4,7 @@ import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
         self.position = pygame.math.Vector2(x, y)
         self.rotation = 0
         self.radius = PLAYER_RADIUS
